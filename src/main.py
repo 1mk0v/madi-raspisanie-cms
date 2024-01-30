dd44d1bfe7954bc70b2f69dff8cd8f7b9993fcd9
1mk0v <potapchuk01@mail.ru>
2024-01-30 13:49:00 +0300

    INIT app

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title='MADI Raspisanie CMS',
+    version="0.1.0",
+    contact={
+        "name": "Potapchuk Danila Antonovich",
+        "url": "https://t.me/nivicki",
+        "email": "potapchuk01@mail.ru"
+    }
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
