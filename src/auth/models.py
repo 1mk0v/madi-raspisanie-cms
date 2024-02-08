7e5cf3f4c5f4a365b4e7c29a91540beb3586ddea
1mk0v <potapchuk01@mail.ru>
2024-02-08 13:19:09 +0300

    Delete user models

--- src/auth/models.py
+++ src/auth/models.py
@@ -7,12 +7,3 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     username: str | None = None
     scopes: list[str] = []
-
-class UsersDB(BaseModel):
-    login:str
-
-class UserDBWithPsw(UsersDB):
-    pwd:str
-
-class UserDBWithHashedPassword(UsersDB):
-    hashed_password:str
