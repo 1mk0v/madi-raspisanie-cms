94925975b607090789f3871ab2365973e52ef039
1mk0v <potapchuk01@mail.ru>
2024-02-08 15:34:01 +0300

    Add returning model to db delete

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -34,12 +34,12 @@ class DatabaseInterface:
         except SQLException.SQLAlchemyError as error:
             raise exc.BaseAPIException(message=error.args, status_code=500)
 
-    async def add(self, data:BaseModel | List[BaseModel]):
+    async def add(self, data:BaseModel):
         try:
             query = (self.schema.insert()
                      .values([data.model_dump()])
                      .returning(self.schema))
-            return self.session.execute(query).fetchall()
+            return self.__model(**self.session.execute(query).fetchone()._mapping)
         except SQLException.SQLAlchemyError as error:
             raise exc.BaseAPIException(message=error.args, status_code=500)
         finally:
