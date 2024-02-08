8cae46f67ea23da18524f5ef2af1297735e5793c
1mk0v <potapchuk01@mail.ru>
2024-02-08 14:52:38 +0300

    Add user name as returned str

--- src/requests/auth.py
+++ src/requests/auth.py
@@ -1,24 +1,15 @@
 from . import BaseRequests
 from bs4 import BeautifulSoup as bs 
-class BazisMadiRequests(BaseRequests):
+class BazisMadiRequests(BaseRequests): 
 
     def __init__(self, url: str = 'https://bazis.madi.ru/{}/login.php') -> None:
         self.none_auth_title = 'Вход в систему'
         super().__init__(url)
 
-    async def login(self, user:str, password:str, url='stud') -> bool:
+    async def login(self, user:str, password:str, url='stud'):
         response = await super()._post(url, {'usr':user,'psw':password})
         result = bs(response.body, 'lxml').find('title')
         if result.text == self.none_auth_title:
             return False
-        return True
-
-    async def get_name_of_user(self, user:str, password:str, url='stud'):
-        response = await super()._post(url, {'usr':user,'psw':password})
         result = bs(response.body, 'lxml').find('div', {'class':'tile-bar'})
-        name = result.text.split(' ')
-        return {
-            'surname': name[0],
-            'name': name[1],
-            'patronymic': name[2]
-        }
+        return result.text
