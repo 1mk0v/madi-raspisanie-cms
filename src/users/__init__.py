f96b54dc29c6c26c8050fb2f181702e2bb1cf862
1mk0v <potapchuk01@mail.ru>
2024-02-08 14:50:02 +0300

    Add users tables interfaces

--- src/users/__init__.py
+++ src/users/__init__.py
@@ -2,7 +2,11 @@ from sqlalchemy import Table
 from database import DatabaseInterface, schemas
 
 class UserTableInterface(DatabaseInterface):
-
     def __init__(self, table_schema: Table = schemas.user, engine=schemas.cms_engine) -> None:
+        super().__init__(table_schema, engine)
+
+
+class UserInfoTableInterface(DatabaseInterface):
+    def __init__(self, table_schema: Table = schemas.user_info, engine=schemas.cms_engine) -> None:
         super().__init__(table_schema, engine)
     
