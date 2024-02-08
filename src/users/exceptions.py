9b656510eada903d390b71e8e328bbd27527f2f7
1mk0v <potapchuk01@mail.ru>
2024-02-08 17:08:59 +0300

    Add new exception

--- src/users/exceptions.py
+++ src/users/exceptions.py
@@ -3,3 +3,7 @@ from exceptions import BaseAPIException
 class DeleteYourselfUserError(BaseAPIException):
     def __init__(self, *args: object, detail: dict = None, message: str = "You can't delete yourself!" , status_code=400) -> None:
         super().__init__(*args, detail=detail, message=message, status_code=status_code)
+
+class DeleteHigherRankingUserError(BaseAPIException):
+    def __init__(self, *args: object, detail: dict = None, message: str = "You cannot delete a user higher than you in rank!" , status_code=400) -> None:
+        super().__init__(*args, detail=detail, message=message, status_code=status_code)
