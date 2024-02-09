d1854c9b36d46cce75f98193ce30603e8e7a2d29
1mk0v <potapchuk01@mail.ru>
2024-02-09 15:30:27 +0300

    Add priority to regist user

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from users.models import UserDBWithPsw, UserRegist, UsersDB
+from users import router as user_router
 from .auth import Authenticator
 from .models import Token
 from .dependencies import oauth2_scheme
@@ -27,10 +28,12 @@ async def auth_user(
         )
     
 @router.post('/registration', dependencies=[oauth2_scheme])
-async def register_user(user:UserRegist, use_bazis:bool = False) -> UsersDB:
+async def register_user(user:UserRegist,
+                        current_user_type: Annotated[dict, Depends(user_router.get_current_user_type)],
+                        use_bazis:bool = False) -> UsersDB:
     try:
         auth = Authenticator()
-        return await auth.regist_user(user, use_bazis)
+        return await auth.regist_user(user, use_bazis, current_user_type)
     except BaseAPIException as error:
         raise HTTPException(
             status_code=error.status_code,
