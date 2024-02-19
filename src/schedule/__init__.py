0bb21d80e9df97367eb2a623a85251dfa8a76a38
1mk0v <potapchuk01@mail.ru>
2024-02-19 17:47:48 +0300

    Add new func + new param ignored types

--- src/schedule/__init__.py
+++ src/schedule/__init__.py
@@ -1,45 +1,48 @@
 from sqlalchemy import Engine, Table
 from database import DatabaseInterface, schemas
 import datetime
-from pydantic import BaseModel
 
 class ScheduleTableInterface(DatabaseInterface):
     def __init__(self, table_schema: Table = schemas.schedule_info, engine: Engine=schemas.schedule_engine) -> None:
-        self.auditorium = DatabaseInterface(schemas.auditorium, schemas.schedule_engine)
-        self.date = DatabaseInterface(schemas.date, schemas.schedule_engine)
+        self.auditorium = DatabaseInterface(schemas.auditorium, schemas.schedule_engine, ['id', 'department_id'])
+        self.date = DatabaseInterface(schemas.date, schemas.schedule_engine, ['id'])
         self.department = DatabaseInterface(schemas.department, schemas.schedule_engine)
-        self.discipline = DatabaseInterface(schemas.discipline, schemas.schedule_engine)
+        self.discipline = DatabaseInterface(schemas.discipline, schemas.schedule_engine, ['id'])
         self.exam_info = DatabaseInterface(schemas.exam_info, schemas.schedule_engine)
-        self.frequency = DatabaseInterface(schemas.frequency, schemas.schedule_engine)
-        self.group = DatabaseInterface(schemas.group, schemas.schedule_engine)
-        self.teacher = DatabaseInterface(schemas.teacher, schemas.schedule_engine)
-        self.time = DatabaseInterface(schemas.time, schemas.schedule_engine)
-        self.type = DatabaseInterface(schemas.type, schemas.schedule_engine)
-        self.weekday = DatabaseInterface(schemas.weekday, schemas.schedule_engine)
-        super().__init__(table_schema, engine)
+        self.frequency = DatabaseInterface(schemas.frequency, schemas.schedule_engine, ['id'])
+        self.group = DatabaseInterface(schemas.group, schemas.schedule_engine, ['year'])
+        self.teacher = DatabaseInterface(schemas.teacher, schemas.schedule_engine, ['year'])
+        self.time = DatabaseInterface(schemas.time, schemas.schedule_engine, ['id'])
+        self.type = DatabaseInterface(schemas.type, schemas.schedule_engine, ['id'])
+        self.weekday = DatabaseInterface(schemas.weekday, schemas.schedule_engine, ['id'])
+        super().__init__(table_schema, engine, ignore_keys=['id'])
 
     #DONT WORK
-    async def add(self, schedule):
+    async def _get_data(self, schedule):
         data = {}
         for field in schedule:
             nameOfField = field[0]
             fieldData = field[1]
             if type(fieldData) not in [str, int, datetime.time]:
                 databaseInterface:DatabaseInterface = self.__dict__[nameOfField]
-                this_data = await self.add(fieldData)
-                print(databaseInterface.model, this_data)
                 data[f'{nameOfField}_id'] = await databaseInterface.select_or_add(
-                    databaseInterface.model(
-                        **this_data
-                    )
+                    await self._get_data(fieldData)
                 )
             else:
-                data[f'{nameOfField}'] = fieldData
-        
-        print(data)
+                if nameOfField in ['frequency']:
+                    databaseInterface:DatabaseInterface = self.__dict__[nameOfField]
+                    data[f'{nameOfField}_id'] = await databaseInterface.select_or_add({'value':fieldData})
+                else:
+                    data[f'{nameOfField}'] = fieldData
         return data
-        
-            # databaseInterface:DatabaseInterface = self.__dict__[nameOfField]
-                # print(databaseInterface.model)
-            # data[f'{nameOfField}_id'] = await databaseInterface.add(fieldData)
-        # query = self.schema.insert().values(data)
+    
+    async def add(self, schedule):
+        data = await self._get_data(schedule)
+        new_data = dict()
+        for key in data:
+            if type(data[key]) == str:
+                databaseInterface:DatabaseInterface = self.__dict__[key]
+                new_data[f'{key}_id'] = await databaseInterface.select_or_add({'value':data[key]})
+            else:
+                new_data[key] = data[key]
+        return await super().add(self.model(**new_data))
