89972d1f5f9f823124c4fb21497768ed4e3984f8
1mk0v <potapchuk01@mail.ru>
2024-02-20 17:29:26 +0300

    Deleted departments

--- src/departments/__init__.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import Engine, Table
-from database import DatabaseInterface, schemas
-
-class DepartmentTableInterface(DatabaseInterface):
-    def __init__(self, table_schema: Table = schemas.department, engine: Engine=schemas.schedule_engine) -> None:
-        super().__init__(table_schema, engine)
