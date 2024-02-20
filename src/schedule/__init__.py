9c505a791bb1bb5b431a20760fadecacd07088a1
1mk0v <potapchuk01@mail.ru>
2024-02-20 16:35:00 +0300

    Change methods to add schedule

--- src/schedule/__init__.py
+++ src/schedule/__init__.py
@@ -1,13 +1,15 @@
 from typing import List
 from pydantic import BaseModel
 from sqlalchemy import Engine, Table
+import sqlalchemy.exc as SQLException
+import exceptions as exc
 from database import DatabaseInterface, schemas
-import datetime
+from date import DateTableInterface
 
 class ScheduleTableInterface(DatabaseInterface):
     def __init__(self, table_schema: Table = schemas.schedule_info, engine: Engine=schemas.schedule_engine) -> None:
         self.auditorium = DatabaseInterface(schemas.auditorium, schemas.schedule_engine, ['id', 'department_id'])
-        self.date = DatabaseInterface(schemas.date, schemas.schedule_engine, ['id'])
+        self.date = DateTableInterface()
         self.department = DatabaseInterface(schemas.department, schemas.schedule_engine)
         self.discipline = DatabaseInterface(schemas.discipline, schemas.schedule_engine, ['id'])
         self.exam_info = DatabaseInterface(schemas.exam_info, schemas.schedule_engine)
@@ -22,46 +24,45 @@ class ScheduleTableInterface(DatabaseInterface):
     async def _getByRowId(self, row):
         return {
             "date": (await self.date.get_by_column('id',row.date_id))[0],
-            "discipline": (await self.discipline.get_by_column('id', row.discipline_id))[0].value,
-            "type": (await self.type.get_by_column('id', row.type_id))[0].value,
-            "auditorium": (await self.auditorium.get_by_column('id', row.auditorium_id))[0].value,
+            "discipline": (await self.discipline.get_by_column('id', row.discipline_id))[0],
+            "type": (await self.type.get_by_column('id', row.type_id))[0],
+            "auditorium": (await self.auditorium.get_by_column('id', row.auditorium_id))[0],
             "teacher": (await self.teacher.get_by_column('id', row.teacher_id))[0],
             "group": (await self.group.get_by_column('id', row.group_id))[0],
-            "weekday": (await self.weekday.get_by_column('id', row.weekday_id))[0].value,
+            "weekday": (await self.weekday.get_by_column('id', row.weekday_id))[0],
         }
 
+    async def get_by_row(self, row:BaseModel):
+        try:
+            query = self.schema.select().where(
+                self.schema.c['date_id'] == row.date_id,
+                self.schema.c['group_id'] == row.group_id,
+                self.schema.c['teacher_id'] == row.teacher_id,
+                self.schema.c['weekday_id'] == row.weekday_id,
+                self.schema.c['discipline_id'] == row.discipline_id,
+                self.schema.c['type_id'] == row.type_id,
+                self.schema.c['auditorium_id'] == row.auditorium_id,
+            )
+            return self.session.execute(query).fetchone()
+        except SQLException.SQLAlchemyError as error:
+            raise exc.BaseAPIException(message=error.args, status_code=500)
+    
     async def get(self, limit: int = 10, offset: int = 0) -> List[BaseModel]:
         data = list()
         for lesson in await super().get(limit, offset):
             data.append(await self._getByRowId(lesson))
         return data
     
-
-    async def _get_data(self, schedule):
-        data = {}
-        for field in schedule:
-            nameOfField = field[0]
-            fieldData = field[1]
-            if type(fieldData) not in [str, int, datetime.time]:
-                databaseInterface:DatabaseInterface = self.__dict__[nameOfField]
-                data[f'{nameOfField}_id'] = await databaseInterface.select_or_add(
-                    await self._get_data(fieldData)
-                )
-            else:
-                if nameOfField in ['frequency']:
-                    databaseInterface:DatabaseInterface = self.__dict__[nameOfField]
-                    data[f'{nameOfField}_id'] = await databaseInterface.select_or_add({'value':fieldData})
-                else:
-                    data[f'{nameOfField}'] = fieldData
-        return data
-    
     async def add(self, schedule):
-        data = await self._get_data(schedule)
-        new_data = dict()
-        for key in data:
-            if type(data[key]) == str:
-                databaseInterface:DatabaseInterface = self.__dict__[key]
-                new_data[f'{key}_id'] = await databaseInterface.select_or_add({'value':data[key]})
-            else:
-                new_data[key] = data[key]
-        return await super().add(self.model(**new_data))
+        data = {
+            "date_id": await self.date.select_or_add(schedule.date),
+            "group_id": await self.group.select_or_add(schedule.group, key="id"),
+            "teacher_id": await self.teacher.select_or_add(schedule.teacher, key="id"),
+            "weekday_id": await self.weekday.select_or_add(schedule.weekday, key=None),
+            "discipline_id": await self.weekday.select_or_add(schedule.discipline, key=None),
+            "type_id": await self.weekday.select_or_add(schedule.type, key=None),
+            "auditorium_id": await self.weekday.select_or_add(schedule.auditorium, key=None)
+        }
+        validated_data = self.model.parse_obj(data)
+        row_of_added_data = await self.get_by_row(validated_data)
+        return row_of_added_data._mapping if row_of_added_data else await super().add(validated_data)
