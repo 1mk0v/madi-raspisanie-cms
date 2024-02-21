3378edeaf316cbac55b17ea9f7096b0bfeda6750
1mk0v <potapchuk01@mail.ru>
2024-02-21 16:19:57 +0300

    Add models

--- src/users/router.py
+++ src/users/router.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, HTTPException, Depends
 from auth import dependencies, config, models, exceptions
 from exceptions import BaseAPIException
 from jose import JWTError, jwt
-from typing import Annotated
+from typing import Annotated, List
+from utils import get_pydantic_model
+from database import DatabaseInterface, schemas
 from . import UserTableInterface, UserInfoTableInterface
 from .exceptions import DeleteYourselfUserError, DeleteHigherRankingUserError
 
@@ -12,7 +14,11 @@ router = APIRouter(
     dependencies=[dependencies.oauth2_scheme]
 )
 
-@router.get('/')
+UserModel = get_pydantic_model(UserTableInterface())
+UserInfoModel = get_pydantic_model(UserInfoTableInterface())
+UserTypeModel = get_pydantic_model(DatabaseInterface(schemas.user_type, schemas.cms_engine))
+
+@router.get('/', response_model=List[UserModel])
 async def get_users(limit:int=10, offset:int=0):
     try:
         usersTable = UserTableInterface()
@@ -49,8 +55,7 @@ async def get_current_user_type(current_user:Annotated[dict, Depends(get_current
             detail=error.message
         )
 
-
-@router.get('/me')
+@router.get('/me', response_model=UserModel)
 async def get_me(current_user:Annotated[dict, Depends(get_current_user)]):
     try:
         return current_user
@@ -59,8 +64,8 @@ async def get_me(current_user:Annotated[dict, Depends(get_current_user)]):
             status_code=error.status_code,
             detail=error.message
         )
-    
-@router.get('/me/type')
+
+@router.get('/me/type', response_model=UserTypeModel)
 async def get_me(type:Annotated[dict, Depends(get_current_user_type)]):
     try:
         return type
@@ -69,7 +74,6 @@ async def get_me(type:Annotated[dict, Depends(get_current_user_type)]):
             status_code=error.status_code,
             detail=error.message
         )
-    
 
 @router.delete('/delete')
 async def delete_user(id:int, current_user:Annotated[dict, Depends(get_current_user)]):
