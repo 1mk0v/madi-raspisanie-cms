8ca88a75e7399ae1ef0be9cd4aec7e4c3d4aa4e3
1mk0v <potapchuk01@mail.ru>
2024-02-21 15:13:21 +0300

    Init new router

---
+++ src/schedule_time/router.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, HTTPException
+from utils import get_pydantic_model
+from auth import dependencies
+from exceptions import BaseAPIException
+from . import TimeTableInterface
+from typing import List
+
+router = APIRouter(
+    prefix='/time',
+    tags=['Time'],
+    dependencies=[dependencies.oauth2_scheme]
+)
+
+model = get_pydantic_model(TimeTableInterface(ignore_keys=[]))
+
+@router.get('/', response_model=List[model])
+async def get_date(limit:int = 10, offset:int = 0):
+    try:
+        time_table = TimeTableInterface()
+        return await time_table.get(limit=limit, offset=offset)
+    except BaseAPIException as error:
+        raise HTTPException(
+            status_code=error.status_code,
+            detail=error.message
+        )
+
+@router.post('/add', response_model=model)
+async def add_date(data:model): #type: ignore
+    try:
+        time_table = TimeTableInterface()
+        return await time_table.add(data)
+    except BaseAPIException as error:
+        raise HTTPException(
+            status_code=error.status_code,
+            detail=error.message
+        )
+
+@router.delete('/delete/{id}', response_model=int)
+async def delete_schedule(id:int):
+    try:
+        time_table = TimeTableInterface()
+        return await time_table.delete(value=id)
+    except BaseAPIException as error:
+        raise HTTPException(
+            status_code=error.status_code,
+            detail=error.message
+        )
